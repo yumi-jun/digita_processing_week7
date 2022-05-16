abb2148ccfd62e21e6b10f4d22c5d797661ece11
DESKTOP-9CMK7U2\관리자 <junyumi1006@ajou.ac.kr>
2022-05-16 22:33:38 +0900

    homo 필터 구현

--- Opencv/Opencv.cpp
+++ Opencv/Opencv.cpp
@@ -1,16 +1,16 @@
-﻿// Opencv.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
-#include <opencv2/highgui.hpp>
+﻿#include <opencv2/highgui.hpp>
 using namespace cv;
 
 /*
 * Homomorphic Filtering
 * 영상의 픽셀 강도는 크게 두가지 영향을 받음
-* 1. 조명과 같은 직접적인 광원 빛 i(x,y)
+* 1. 조명과 같은 직접적인 광원 빛 i(x,y) -> 천천히 바뀜,, 앞에 있음
 * 2. 객체로부터 반사된 빛 r(x,y)
 * 디지털 영상은 f(x,y)=i(x,y),r(x,y)로 표현 가능
 * 자연 로그를 이용하여 곱을 합으로 표현함.
+* 
+* 조명의 빛을 없애고 싶음 .. 조명의 빛은 천천히? 바뀔테니.. 위쪽 영향을 빼주자!!
+* 
 * 디지털 영상의 조명과 반사 성분 각각 필터링하여 출력 영상 g(x,y)를 얻는 것임.
 */
 void homomorphicFilter(InputArray input, OutputArray output, float D0, float gammaH, float gammaL)
@@ -18,7 +18,36 @@ void homomorphicFilter(InputArray input, OutputArray output, float D0, float gam
     const Mat& src = input.getMat();
     output.create(input.size(), CV_32FC1);
     Mat dst = output.getMat();
-    Mat F;
+    Mat F,srcLn;
+    cv::log(src + 1,srcLn);
+    //푸리에 변환,  로그를 취해줌
+    dft(srcLn, F, DFT_COMPLEX_OUTPUT);
+    float c = 1;
+    //필터링
+    for (int u = 0; u < F.rows; u++) {
+        for(int v=0;v<F.cols;v++){
+    
+            float D2 = 0;
+            if (u < F.rows / 2) { D2 += u * u; }
+            else { D2 += (F.rows - u) * (F.rows - u); }
+            float D2 = 0;
+            if (u < F.cols / 2) { D2 += u * u; }
+            else { D2 += (F.cols - u) * (F.cols - u); }
+
+            float D = u * u + v * v;
+            
+            F.at<float>(u, v) = ((gammaH - gammaL) * (1 - exp(-c * (D * D / D0 / D0))) + gammaL)*F.at<float>(u,v);
+        }
+    }
+    F.at<float>(0, 0) = F.rows * F.cols * 0.5;
+
+   
+    imshow("F", F);
+    //역푸리에 변환
+    idft(F, dst, DFT_REAL_OUTPUT | DFT_SCALE);
+    //지수 
+    exp(dst, dst);
+    dst -= 1;
 
 }
 int main()
@@ -26,20 +55,13 @@ int main()
     //Homomorophic Filter
     Mat input = imread("C:/Users/관리자/Desktop/yumi.jpg", 0);
     Mat output;
+    //input을 float 값으로 바꿔주고 범위를 1부터 255부터 지정해줌
     input.convertTo(input, CV_32F, 1 / 255.f);
-    homomorphicFilter(input, output, 15, 1, 0.4f);
-    imshow("input", input);
+
+    homomorphicFilter(input, output, 10, 1, 0.4f);
+
+    imshow("output", output);
     waitKey();
 
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
